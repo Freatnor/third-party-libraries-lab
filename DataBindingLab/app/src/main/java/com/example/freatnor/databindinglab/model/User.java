fef28f45f70af687ddeba7c1267bc4d0bc31b997
Freatnor <jonathan.randall.taylor@gmail.com>
2016-08-24 14:57:57 -0400

    starting on lab

---
+++ DataBindingLab/app/src/main/java/com/example/freatnor/databindinglab/model/User.java
@@ -0,0 +1,26 @@
+package com.example.freatnor.databindinglab.model;
+
+/**
+ * Created by Jonathan Taylor on 8/24/16.
+ */
+public class User {
+    private String mUserName;
+    private String mDepartment;
+    private int mAge;
+
+    public String getmUserName() {
+        return mUserName;
+    }
+
+    public String getmDepartment() {
+        return mDepartment;
+    }
+
+    public int getmAge() {
+        return mAge;
+    }
+
+    public void setmAge(int mAge) {
+        this.mAge = mAge;
+    }
+}
---
+++ DataBindingLab/app/src/main/java/com/example/freatnor/databindinglab/presenter/DataBindingAdapter.java
@@ -0,0 +1,27 @@
+package com.example.freatnor.databindinglab.presenter;
+
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.view.ViewGroup;
+
+/**
+ * Created by Jonathan Taylor on 8/24/16.
+ */
+public class DataBindingAdapter extends RecyclerView.Adapter {
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
